a7e11a1637d86dfe62631c9c4577c8bf6dc396a3
y86071971-stack <y86071971@gmail.com>
2026-02-19 14:52:27 +0300

    Update add.py

--- add.py
+++ add.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
-model = joblib.load("C:/Users/PC1/Desktop/fares/model_to_fares.pkl")
-sc= joblib.load("C:/Users/PC1/Desktop/fares/sc_to_fares.pkl")
+model = joblib.load("model_to_fares.pkl")
+sc= joblib.load("sc_to_fares.pkl")
 water=st.number_input("water:",step=1.00)
 
 PerCapit=st.number_input("Per Capit:",step=1.00)
@@ -19,4 +19,5 @@ GroundwaterDepletionRate=st.number_input("Groundwater Depletion Rate:",step=1.00
 
 input_scale=sc.transform([[water,PerCapit,AgriculturalWaterUse,IndustrialWaterUse,HouseholdWaterUse,RainfallImpact,GroundwaterDepletionRate]])
 WaterScarcityLevel=model.predict(input_scale)
+
 st.success(WaterScarcityLevel)
